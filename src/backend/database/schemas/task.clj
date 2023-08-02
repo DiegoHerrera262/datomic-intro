525bcb7c30bc9c6d3a3bf77dc9034c989f7d939b
Dataico <dataico@Alejandros-Mac-mini.local>
2023-08-02 16:34:35 -0500

    Changed uuid of responsible user

--- src/backend/database/schemas/task.clj
+++ src/backend/database/schemas/task.clj
@@ -17,6 +17,6 @@
                    :db/doc "Status of a task [:pending :done]"}
 
                   {:db/ident :task/assignee
-                   :db/valueType :db.type/ref
+                   :db/valueType :db.type/uuid
                    :db/cardinality :db.cardinality/one
                    :db/doc "User responsible of the task"}])
